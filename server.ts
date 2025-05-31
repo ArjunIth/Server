dea7ca2926f6d0f79506e5092f4d984632f1a8cb
ArjunIth <you@example.com>
2025-05-31 22:17:11 +0545

    first commit of Saas Project2

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import app from "./src/app";
 import { config } from "dotenv";
 config()
 
+import "./src/database/connection"; // database connection file lai import garne
 function startServer(){
     const port = process.env.PORT 
     app.listen(port,function(){
